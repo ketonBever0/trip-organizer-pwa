351d43941ee105c0fd5ce589ea3a0183462cf270
Kurucz Laci <lacikurucz09@gmail.com>
2025-03-17 01:11:16 +0100

    project init

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomeComponent } from './pages/home/home.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: '**', component: NotFoundComponent },
+];
