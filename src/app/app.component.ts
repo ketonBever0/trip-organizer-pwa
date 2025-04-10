0b876f1b5d9bdb0cd94e6abc8b91ccb43b509ead
Kurucz Laci <lacikurucz09@gmail.com>
2025-04-10 14:13:43 +0200

    Offline mode and app update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { interval } from 'rxjs';
+import { SwUpdate } from '@angular/service-worker';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, MatSnackBarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'pwa-project';
+
+  public ngOnDestroy(): void {
+    window.removeEventListener('offline', this.offlineCb);
+    window.removeEventListener('online', this.onlineCb);
+  }
+
+  private offlineCb!: () => void;
+  private onlineCb!: () => void;
+
+  private swUpdate = inject(SwUpdate);
+  private snackBar = inject(MatSnackBar);
+
+  ngOnInit(): void {
+    // this.swUpdate.activateUpdate();
+    interval(3000).subscribe(() => {
+      this.swUpdate.checkForUpdate().then((update) => {
+        if (update) {
+          alert('New version available!');
+          window.location.reload();
+        }
+      });
+
+      this.offlineCb = () => {
+        this.snackBar.open('You are offline!', 'Ok', { duration: 10000 });
+      };
+      this.onlineCb = () => {
+        this.snackBar.open('You are online!', '', { duration: 3000 });
+      };
+      window.addEventListener('offline', this.offlineCb);
+      window.addEventListener('online', this.onlineCb);
+    });
+  }
 }
