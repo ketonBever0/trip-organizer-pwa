8536f35f5a8a92d94db428ba2de476c0ca3901a4
Kurucz Laci <lacikurucz09@gmail.com>
2025-04-10 11:12:09 +0200

    Firebase config removed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -31,14 +31,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideFirebaseApp(() =>
-      initializeApp({
-        apiKey: 'AIzaSyCxaPfiywfMojF8gBf5_mTUIxdRyrQwZSs',
-        authDomain: 'tourism-d7f90.firebaseapp.com',
-        projectId: 'tourism-d7f90',
-        storageBucket: 'tourism-d7f90.firebasestorage.app',
-        messagingSenderId: '1083320592689',
-        appId: '1:1083320592689:web:32b473bc39ffc1d7b96810',
-      })
+      initializeApp(env.firebaseConfig)
     ),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
